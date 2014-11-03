c199fc069d8612398bb4776c6b0b75dc72cb28f1
保坂 駿 <reyurnible@gmail.com>
2014-11-03 22:07:22 +0900

    Fix image loading error

--- Tabemon/src/com/reyurnible/tabemon/DetailsActivity.java
+++ Tabemon/src/com/reyurnible/tabemon/DetailsActivity.java
@@ -13,7 +13,9 @@ import com.reyurnible.network.VolleyHelper;
 import com.reyurnible.object.Recipe;
 import com.reyurnible.view.CustomListDialogCallback;
 import com.reyurnible.view.CustomListDialogFragment;
+import com.reyurnible.view.RoundedTransformation;
 import com.reyurnible.tabemon.R;
+import com.squareup.picasso.Picasso;
 
 import android.app.Activity;
 import android.app.AlarmManager;
@@ -31,29 +33,17 @@ public class DetailsActivity extends Activity implements OnClickListener, Custom
 	//Intent key
 	public static final String IntentRecipeJSON ="string_RecipeJSON";
 	
-	private ImageView thumnailImage;
-	private TextView nameText;
-	private TextView materialsText;
-	private TextView moneyText;
-	private TextView timeText;
-	//private Button recipeButton;
-	private Button makeButton;
-	private Button remindButton;
-	//private Button moreButton;
-	
-	private Recipe recipe;
-	private String recipeJSON;
-	private static CustomImageLoader imageLoader;
+	private Recipe mRecipe;
+	private String mRecipeJSON;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_details);
-		findViews();
 		Intent intent = getIntent();
-		recipeJSON = intent.getStringExtra(IntentRecipeJSON);
+		mRecipeJSON = intent.getStringExtra(IntentRecipeJSON);
 		try {
-			recipe = TranslateJSON.recipeParse(new JSONObject(recipeJSON));
+			mRecipe = TranslateJSON.recipeParse(new JSONObject(mRecipeJSON));
 			setViews();
 		} catch (JSONException e) {
 			e.printStackTrace();
@@ -61,57 +51,47 @@ public class DetailsActivity extends Activity implements OnClickListener, Custom
 		}
 	}
 	
-	private void findViews(){
-		thumnailImage = (ImageView)findViewById(R.id.details_ImageViewThumnails);
-		nameText = (TextView)findViewById(R.id.details_TextViewName);
-		materialsText = (TextView)findViewById(R.id.details_TextViewMaterials);
-		moneyText = (TextView)findViewById(R.id.details_TextViewMoney);
-		timeText = (TextView)findViewById(R.id.details_TextViewTime);
-		//recipeButton = (Button)findViewById(R.id.details_ButtonRecipe);
-		makeButton = (Button)findViewById(R.id.details_ButtonMake);
-		remindButton = (Button)findViewById(R.id.details_ButtonRemind);
-		//moreButton = (Button)findViewById(R.id.details_ButtonMore);
-	}
 	private void setViews(){
-		imageLoader = VolleyHelper.getImageLoader(getApplicationContext());
-		ImageListener listener = CustomImageLoader.getImageListener(thumnailImage,
-				  R.drawable.bg_image_loading,/* 表示待ち時の画像 白いだけの画像*/
-				  R.drawable.failed_image /* エラー時の画像 */);
-		imageLoader.get(recipe.foodImageUrl,Priority.HIGH,listener); /* URLから画像を取得する */
+		Picasso.with(getApplicationContext())
+		    .load(mRecipe.foodImageUrl)
+		    .placeholder(R.drawable.bg_image_loading)
+		    .error(R.drawable.failed_image)
+		    .transform(new RoundedTransformation(32, 0))
+		    .into((ImageView) findViewById(R.id.details_ImageViewThumnails));
 		
-		nameText.setText(recipe.recipeTitle);
+		((TextView) findViewById(R.id.details_TextViewName)).setText(mRecipe.recipeTitle);
+		
+		TextView materialsText = (TextView) findViewById(R.id.details_TextViewMaterials);
 		StringBuilder materialsBuilder = new StringBuilder();
-		for(String material : recipe.recipeMaterial){
+		for(String material : mRecipe.recipeMaterial){
 			materialsBuilder.append(material);
 			materialsBuilder.append("\n");
 		}
 		materialsText.setText(materialsBuilder.toString());
-		moneyText.setText(recipe.recipeCost);
-		timeText.setText(recipe.recipeIndication);
-		//recipeButton.setOnClickListener(this);
-		makeButton.setOnClickListener(this);
-		remindButton.setOnClickListener(this);
-		//moreButton.setOnClickListener(this);
+		((TextView) findViewById(R.id.details_TextViewMoney)).setText(mRecipe.recipeCost);
+		((TextView) findViewById(R.id.details_TextViewTime)).setText(mRecipe.recipeIndication);
+
+		findViewById(R.id.details_ButtonMake).setOnClickListener(this);
+		findViewById(R.id.details_ButtonRemind).setOnClickListener(this);
 	}
 	@Override
-	protected void onStart(){
+	protected void onStart() {
 		super.onStart();
 		VolleyHelper.RequestStart();
 	}
+	
 	@Override
-	protected void onStop(){
+	protected void onStop() {
 		super.onStop();
 		VolleyHelper.RequestStop();
 		VolleyHelper.RequestCancell("DetailsActivity");
 	}
+	
 	@Override
 	public void onClick(View view) {
 		switch(view.getId()){
-		/*case R.id.details_ButtonRecipe:
-			break;
-		*/
 		case R.id.details_ButtonMake:
-			Uri uri = Uri.parse(recipe.recipeUrl);
+			Uri uri = Uri.parse(mRecipe.recipeUrl);
 			Intent intent = new Intent(Intent.ACTION_VIEW,uri);
 			startActivity(intent);
 			break;
@@ -132,11 +112,10 @@ public class DetailsActivity extends Activity implements OnClickListener, Custom
 	@Override
 	public void onClickedContentButtons(int position) {
 		/*-----------後で通知するようにインテントを設定-----------*/
-		//String[] contents = {"30分後","１時間後","2時間語","4時間語","6時間語"};
 		//受けっとった値で何分後に通知かを設定
 		//呼び出す日時を設定する
 		Calendar triggerTime = Calendar.getInstance();
-		switch(position){
+		switch(position) {
 		case 0:
 			//"30分後"
 			triggerTime.add(Calendar.MINUTE, 30);
@@ -160,7 +139,7 @@ public class DetailsActivity extends Activity implements OnClickListener, Custom
 		}
 		//設定した日時で発行するIntentを生成
 		Intent intent = new Intent(getApplicationContext(), RemindLaterReceiver.class);
-		intent.putExtra(RemindLaterReceiver.IntentRecipeJSON, recipeJSON);
+		intent.putExtra(RemindLaterReceiver.IntentRecipeJSON, mRecipeJSON);
 		PendingIntent sender = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 		//日時と発行するIntentをAlarmManagerにセットします
 		AlarmManager manager = (AlarmManager)getSystemService(ALARM_SERVICE);
@@ -169,7 +148,6 @@ public class DetailsActivity extends Activity implements OnClickListener, Custom
 
 	@Override
 	public void onClickedCloseButton() {
-		// TODO Auto-generated method stub
 		
 	}
 }
--- Tabemon/src/com/reyurnible/tabemon/MainActivity.java
+++ Tabemon/src/com/reyurnible/tabemon/MainActivity.java
@@ -25,6 +25,8 @@ import com.reyurnible.object.Recipe;
 import com.reyurnible.object.Weather;
 import com.reyurnible.tabemon.R;
 import com.reyurnible.view.CustomDialogFragment;
+import com.reyurnible.view.RoundedTransformation;
+import com.squareup.picasso.Picasso;
 
 import android.app.Activity;
 import android.app.FragmentManager;
@@ -49,8 +51,6 @@ public class MainActivity extends Activity implements ConnectionCallbacks , OnCo
 	private LocationClient mLocationClient;
 	private LocationRequest mLocationRequest;
 	private Location mLocation;
-	private static CustomImageLoader imageLoader;
-	private ImageListener listener;
 	//レシピのリスト
 	private ArrayList<Recipe> recipeList;
 	private ArrayList<String> recipeJSONList;
@@ -61,8 +61,6 @@ public class MainActivity extends Activity implements ConnectionCallbacks , OnCo
 	
 	private ImageView characterImage;
 	private ImageView recipeImage;
-	private Button yesButton;
-	private Button noButton;
 	
 	private Timer progressTimer;
 	private android.os.Handler mHandler = new android.os.Handler();
@@ -96,51 +94,49 @@ public class MainActivity extends Activity implements ConnectionCallbacks , OnCo
 	}
 	
 	@Override
-	public void onResume(){
+	public void onResume() {
 		super.onResume();
 		/*通信状態の取得*/
 		networkInfo = connectivityManager.getActiveNetworkInfo();
-		if(networkInfo==null||!networkInfo.isConnected()){
+		if (networkInfo==null||!networkInfo.isConnected()) {
 			FragmentManager manager = getFragmentManager();
 			CustomDialogFragment dialog = new CustomDialogFragment("エラー","ネットワークに接続できません\n通信状態を確認してください。");
 			dialog.show(manager,"network error");
 			isConnect = false;
-		}else{
+		} else {
 			isConnect = true;
 		}
 	}
 	
-	private void findViews(){
+	private void findViews() {
 		characterImage = (ImageView)findViewById(R.id.main_ImageViewCharacter);
 		recipeImage = (ImageView)findViewById(R.id.main_ImageViewThumnail);
-		yesButton = (Button)findViewById(R.id.main_ButtonYes);
-		noButton = (Button)findViewById(R.id.main_ButtonNo);
 	}
-	private void setViews(){
-		yesButton.setOnClickListener(this);
-		noButton.setOnClickListener(this);
+	private void setViews() {
+		findViewById(R.id.main_ButtonYes).setOnClickListener(this);
+		findViewById(R.id.main_ButtonNo).setOnClickListener(this);
 	}
 	
 	@Override
-	protected void onStart(){
+	protected void onStart() {
 		super.onStart();
 		mLocationClient.connect();
 		VolleyHelper.RequestStart();
 		onImageLoad();
 	}
 	@Override
-	protected void onStop(){
+	protected void onStop() {
 		super.onStop();
 		mLocationClient.disconnect();
 		VolleyHelper.RequestStop();
 		VolleyHelper.RequestCancell("MainActivity");		
 	}
-	private void onRequest(){
-		if(!isConnect)return ;
+	private void onRequest() {
+		if (!isConnect)return ;
 		String url;
-		if(weather!=null){
+		if (weather!=null) {
 			url = ManageUri.getRecipes(this,requestPage++,weather);
-		}else{
+		} else {
 			url = ManageUri.getRecipes(this,requestPage++);
 		}
 		Log.d("MainActivity","onRequest"+url);
@@ -152,31 +148,29 @@ public class MainActivity extends Activity implements ConnectionCallbacks , OnCo
 				url,
 				NetworkTaskCallback.RecipesTask);
 	}
-	private void onImageLoad(){
-		
-		Log.d("MainActivity","OnImageLoad");
-		if(recipeIndex<recipeList.size()){
-			imageLoader = VolleyHelper.getImageLoader(getApplicationContext());
-			// リクエストのキャンセル処理
-	        if (recipeList.get(recipeIndex).imageContainer != null) {
-	        	recipeList.get(recipeIndex).imageContainer.cancelRequest();
-	        }
-			listener = CustomImageLoader.getImageListener(
-					recipeImage,
-					R.drawable.bg_image_loading,/* 表示待ち時の画像 白いだけの画像*/
-					R.drawable.failed_image /* エラー時の画像 */);
-			recipeList.get(recipeIndex).imageContainer = imageLoader.get(recipeList.get(recipeIndex++).foodImageUrl,Priority.IMMEDIATE,listener); /* URLから画像を取得する */
-			if(recipeIndex+3>recipeList.size()){
+	private void onImageLoad() {
+		if (recipeIndex<recipeList.size()) {
+			
+			Picasso.with(getApplicationContext())
+					.load(recipeList.get(recipeIndex++).foodImageUrl)
+					.placeholder(R.drawable.bg_image_loading)
+					.error(R.drawable.failed_image)
+					.transform(new RoundedTransformation(32, 0))
+					.into(recipeImage);
+			
+			if (recipeIndex+3>recipeList.size()) {
 				onRequest();
 			}
 		}
 	}
 	//接続出来た時のコールバック
 	@Override
-	public void onConnected(Bundle arg0) {
+	public void onConnected(Bundle arg) {
 		//位置情報の取得開始
-		if(mLocationClient.isConnected()){
+		if (mLocationClient.isConnected()) {
+			mLocation = mLocationClient.getLastLocation();
 			mLocationClient.requestLocationUpdates(mLocationRequest, this);
+			getTemplatureTask();
 		}
 	}
 	//接続失敗時のコールバック
@@ -191,30 +185,24 @@ public class MainActivity extends Activity implements ConnectionCallbacks , OnCo
 	}
 	@Override
 	public void onLocationChanged(Location location) {
-		if(mLocationClient.isConnected()){
+		if (mLocationClient.isConnected()) {
 			//位置情報が取れたらトーストを表示
 			mLocation = location;
-			Log.d("Location",location.toString());
 			mLocationClient.removeLocationUpdates(this);
-			if(!isConnect)return ;
-			NetworkApiTask tempTask = new NetworkApiTask(this, "MainActivity");
-			tempTask.onRequest(
-					VolleyHelper.getRequestQueue(getApplicationContext()), 
-					Method.GET, 
-					Priority.HIGH,
-					ManageUri.getTempalature(location),
-					NetworkTaskCallback.TempalatureTask);
+			if (!isConnect) return ;
+			getTemplatureTask();
 		}
 	}
 	
 	@Override
 	public void onSuccessNetworkTask(int taskId, Object object) {
-		if(taskId == NetworkTaskCallback.TempalatureTask){
+		if (object!=null) {
+			Log.d(getClass().getSimpleName(),object.toString());
+		}
+		if (taskId == NetworkTaskCallback.TempalatureTask) {
 			weather = TranslateJSON.weatherParse((JSONObject)object);
 			onRequest();
-			Log.d("MainActivity","OnSuccessNetworkTask:TempalatureTask");
-		}else if(taskId == NetworkTaskCallback.RecipesTask){
-			Log.d("MainActivity","OnSuccessNetworkTask:RecipesTask");
+		}else if (taskId == NetworkTaskCallback.RecipesTask) {
 			recipeList.addAll(TranslateJSON.recipesParse((JSONObject)object));
 			recipeJSONList.addAll(TranslateJSON.recipesJSONParse((JSONObject)object));
 			onImageLoad();
@@ -223,18 +211,18 @@ public class MainActivity extends Activity implements ConnectionCallbacks , OnCo
 
 	@Override
 	public void onFailedNetworkTask(int taskId) {
-		if(taskId == NetworkTaskCallback.TempalatureTask){
-			
-		}else if(taskId == NetworkTaskCallback.RecipesTask){
-			Log.d("MainActivity","OnFailedNetworkTask:RecipesTask");
+		if (taskId == NetworkTaskCallback.TempalatureTask) {
+			Log.d(getClass().getSimpleName(),"OnFailedNetworkTask:TempalatureTask");
+		}else if (taskId == NetworkTaskCallback.RecipesTask) {
+			Log.d(getClass().getSimpleName(),"OnFailedNetworkTask:RecipesTask");
 		}
 	}
 
 	@Override
 	public void onClick(View view) {
-		switch(view.getId()){
+		switch(view.getId()) {
 		case R.id.main_ButtonYes:
-			if(recipeIndex>0 && !recipeJSONList.isEmpty()){
+			if (recipeIndex>0 && !recipeJSONList.isEmpty()) {
 				Intent intent = new Intent(getApplicationContext(),DetailsActivity.class);
 				intent.putExtra(DetailsActivity.IntentRecipeJSON, recipeJSONList.get(recipeIndex-1));
 				startActivity(intent);
@@ -247,22 +235,34 @@ public class MainActivity extends Activity implements ConnectionCallbacks , OnCo
 		}
 	}
 	
-	public void setTimer(){
+	private void getTemplatureTask(){
+		if(mLocation==null) return;
+		NetworkApiTask tempTask = new NetworkApiTask(this, "MainActivity");
+		tempTask.onRequest(
+				VolleyHelper.getRequestQueue(getApplicationContext()), 
+				Method.GET, 
+				Priority.HIGH,
+				ManageUri.getTempalature(mLocation),
+				NetworkTaskCallback.TempalatureTask);
+	}
+	
+	
+	public void setTimer() {
 		// タイマーの設定
      	progressTimer = new Timer(false);
-     	progressTimer.schedule(new TimerTask(){
+     	progressTimer.schedule(new TimerTask() {
      		@Override
      		public void run() {
      			mHandler.post(new Runnable() {
      				@Override
      				public void run() {
-     					if(recipeList.size()>0){
+     					if (recipeList.size()>0) {
      						characterImage.setImageResource(R.drawable.character);
      						progressTimer.cancel();
-     					}else{
-     						if(loadImageNum==0){
+     					} else {
+     						if (loadImageNum==0) {
      							characterImage.setImageResource(R.drawable.character_load1);
-     						}else{
+     						} else {
      							characterImage.setImageResource(R.drawable.character_load2);
      						}
      						loadImageNum = (loadImageNum+1)%2;
